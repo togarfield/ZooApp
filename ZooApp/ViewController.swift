9396a1b9c4c602db54f2efea3974b8b3ed538ea3
mabduhsiregar <mabduhsiregar@gmail.com>
2018-08-15 22:37:39 +0700

    Initial Commit

---
+++ ZooApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZooApp
+//
+//  Created by Muhammad abduh Siregar on 15/08/18.
+//  Copyright © 2018 Muhammad abduh Siregar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ZooAppTests/ZooAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  ZooAppTests.swift
+//  ZooAppTests
+//
+//  Created by Muhammad abduh Siregar on 15/08/18.
+//  Copyright © 2018 Muhammad abduh Siregar. All rights reserved.
+//
+
+import XCTest
+@testable import ZooApp
+
+class ZooAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
